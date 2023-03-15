41757747925dabef88fe63f44267420f4cb35911
人菜瘾大 <ccxiaoy@outlook.com>
2023-03-15 23:15:09 +0800

    feature: use mqtt protocol and cloud server to suport control remotely

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-const wifi = require('Wifi');
-const http = require('http');
+const wifi = require("Wifi");
+const http = require("http");
+const mqtt = require("modules/MQTT.min.js");
 
 // wifi 名称
-const WIFI_Name = "where is your dick";
+// const WIFI_NAME = "where is your dick";
+const WIFI_NAME = "abcdefg";
 // wifi 参数
 const WIFI_OPTIONS = {
-    password: "boyulinzuishuai"
+  password: "12345678",
+  // password: "boyulinzuishuai",
 };
 console.log("connecting...");
-// yilianjiewifi的事件
-wifi.on('connected', function () {
-    wifi.getIP((err, info) => {
-        if(err !== null) {
-            throw err;
-        }
-        // 获取一下所在wifi下的局域网IP地址
-        console.log(info.ip, 'connected');
-        startServer();
-    })
-})
+//已连接wifi的事件
+wifi.on("connected", function () {
+  wifi.getIP((err, info) => {
+    if (err !== null) {
+      throw err;
+    }
+    //获取一下所在wifi下的局域网IP地址
+    console.log(info.ip, "connected");
+    connectMQTTServer();
+  });
+});
 // 连接wifi
-wifi.connect(WIFI_Name, WIFI_OPTIONS, err => {
-    if(err !== null) {
-        throw err;
-    }
-})
-// 开启服务的函数
-function startServer() {
-    let PORT = 1314;
-    http.createServer(serverCallback).listen(PORT);
-    console.log(`server is running listen ${PORT}`);
-}
+wifi.connect(WIFI_NAME, WIFI_OPTIONS, (err) => {
+  if (err !== null) {
+    throw err;
+  }
+});
 
-// 服务的回调函数
-function serverCallback(req, res) {
-    // 使用 url 模块对请求地址进行解析
-    const URL = url.parse(req.url, true);
-    if (req.method === 'POST' && URL.pathname == '/led') {
-        let data = "";
-        req.on('data', (chunk) => {
-            data += chunk;
-        })
-        req.on('end', () => {
-            const params = JSON.parse(data);
-            // 0 和 1 
-            // 0是低电平开启灯 
-            // 1是高电平关闭灯
-            console.log(params);
-            digitalWrite(NodeMCU.D4, params.status);
-            res.end('ok');
-        })
-    } else {
-        return res.end('err');
+// 连接mqtt服务的函数
+function connectMQTTServer() {
+  // 139.224.56.89 cloud server open ip address
+  // var host = "139.224.56.89";
+  var host = "localhost/";
+  var client = mqtt.connect({
+    host: host,
+    port: 1883,
+    username: "user",
+    password: "123456",
+  });
+  client.on("connect", () => {
+    console.log("connected success ...");
+    client.subscribe("topic/led", { qos: 1 });
+  });
+  client.on("message", (topic, message) => {
+    console.log("message success ...");
+    // console.log(pub,'pub')
+    if (topic == "topic/led") {
+      var params = message.toString();
+      params = JSON.parse(params);
+      digitalWrite(NodeMCU.D4, params.status);
     }
+  });
 }
